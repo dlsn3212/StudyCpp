63c879ac2e7eee9c129c489cba5af98cadebf531
dlsn3212 <dywjd2341@naver.com>
2020-04-24 11:15:47 +0900

    4/24 5장완료

---
+++ Chap02App/Chap05App/TimeOpPlus.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+class Time {
+	friend const Time operator+(const Time& me, const Time& other);
+private:
+	int hour, min, sec;
+public:
+	Time() { ; }
+	Time(int h, int m, int s) : hour(h), min(m), sec(s) { ; }
+	void OutTime() {
+		printf("%d:%d:%d\n", hour, min, sec);
+	}
+	
+};
+
+const Time operator +(const Time& me, const Time& other){
+	Time t;
+	t.sec = me.sec + other.sec; //35+56=91/ 60=1(min)31(sec)
+	t.min = me.min + other.min;
+	t.hour = me.hour + other.hour;
+
+	t.min = t.min + t.sec / 60;
+	t.sec = t.sec % 60;
+	t.hour += t.min / 60;
+	t.min %= 60;
+	return t;
+}
+int main()
+{
+	Time t1(1, 40, 32), t2(2, 21, 33);
+	Time t3;
+	t1.OutTime();
+	t2.OutTime();
+	t3 = t1 + t2;
+	t3.OutTime();
+}
